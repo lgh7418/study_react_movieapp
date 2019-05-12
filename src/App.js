822ee1e6896e0609e7a15bec251c35550c9dc553
lgh7418 <lgh7418@gmial.com>
2019-05-12 21:45:15 +0900

    8. Validating Props with Prop Types

--- src/App.js
+++ src/App.js
@@ -2,22 +2,32 @@ import React from "react";
 import "./App.css";
 import Movie from "./Movie";
 
-const movieTitles = ["Matrix", "The Hunger Games", "Oldboy", "Star Wars"];
-
-const movieImages = [
-  "https://images-na.ssl-images-amazon.com/images/I/51vpnbwFHrL._SY445_.jpg",
-  "https://upload.wikimedia.org/wikipedia/en/1/12/Catching-Fire_poster.jpg",
-  "http://www.zonalibre.org/blog/clone/archives/old-boy-2.jpg",
-  "https://images-na.ssl-images-amazon.com/images/I/51QvoX064kL._SX303_BO1,204,203,200_.jpg"
+const movies = [
+  {
+    title: "Matrix",
+    poster: "https://images-na.ssl-images-amazon.com/images/I/51vpnbwFHrL._SY445_.jpg"
+  },
+  {
+    title: "The Hunger Games",
+    poster: "https://upload.wikimedia.org/wikipedia/en/1/12/Catching-Fire_poster.jpg"
+  },
+  {
+    title: "Oldboy",
+    poster: "http://www.zonalibre.org/blog/clone/archives/old-boy-2.jpg"
+  },
+  {
+    title: "Star Wars",
+    poster:
+      "https://images-na.ssl-images-amazon.com/images/I/51QvoX064kL._SX303_BO1,204,203,200_.jpg"
+  }
 ];
 
 function App() {
   return (
     <div className="App">
-      <Movie title={movieTitles[0]} poster={movieImages[0]} />
-      <Movie title={movieTitles[1]} poster={movieImages[1]} />
-      <Movie title={movieTitles[2]} poster={movieImages[2]} />
-      <Movie title={movieTitles[3]} poster={movieImages[3]} />
+      {movies.map((movie, index) => {
+        return <Movie title={movie.title} poster={movie.poster} key={index} />;
+      })}
     </div>
   );
 }
