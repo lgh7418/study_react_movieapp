bf4fddc9222749e139d9fec53bf76acb32c38cfb
lgh7418 <lgh7418@gmial.com>
2019-05-12 15:11:47 +0900

    5. Data flow with Props

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Movie from "./Movie";
+
+const movieTitles = ["Matrix", "The Hunger Games", "Oldboy", "Star Wars"];
+
+const movieImages = [
+  "https://images-na.ssl-images-amazon.com/images/I/51vpnbwFHrL._SY445_.jpg",
+  "https://upload.wikimedia.org/wikipedia/en/1/12/Catching-Fire_poster.jpg",
+  "http://www.zonalibre.org/blog/clone/archives/old-boy-2.jpg",
+  "https://images-na.ssl-images-amazon.com/images/I/51QvoX064kL._SX303_BO1,204,203,200_.jpg"
+];
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Movie title={movieTitles[0]} poster={movieImages[0]} />
+      <Movie title={movieTitles[1]} poster={movieImages[1]} />
+      <Movie title={movieTitles[2]} poster={movieImages[2]} />
+      <Movie title={movieTitles[3]} poster={movieImages[3]} />
     </div>
   );
 }
