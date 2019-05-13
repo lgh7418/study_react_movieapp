23df928f763780978168a30d267318ba2c31aa28
lgh7418 <lgh7418@gmial.com>
2019-05-13 18:20:58 +0900

    15. Promises

--- src/App.js
+++ src/App.js
@@ -6,34 +6,10 @@ class App extends Component {
   state = {};
 
   componentDidMount() {
-    setTimeout(() => {
-      this.setState({
-        movies: [
-          {
-            title: "Matrix",
-            poster: "https://images-na.ssl-images-amazon.com/images/I/51vpnbwFHrL._SY445_.jpg"
-          },
-          {
-            title: "The Hunger Games",
-            poster: "https://upload.wikimedia.org/wikipedia/en/1/12/Catching-Fire_poster.jpg"
-          },
-          {
-            title: "Oldboy",
-            poster: "http://www.zonalibre.org/blog/clone/archives/old-boy-2.jpg"
-          },
-          {
-            title: "Star Wars",
-            poster:
-              "https://images-na.ssl-images-amazon.com/images/I/51QvoX064kL._SX303_BO1,204,203,200_.jpg"
-          },
-          {
-            title: "Zootopia",
-            poster:
-              "https://upload.wikimedia.org/wikipedia/ko/thumb/7/75/%EC%A3%BC%ED%86%A0%ED%94%BC%EC%95%84.jpg/250px-%EC%A3%BC%ED%86%A0%ED%94%BC%EC%95%84.jpg"
-          }
-        ]
-      });
-    }, 3000);
+    fetch("https://yts.am/api/v2/list_movies.json?sort_by=rating")
+      .then(response => response.json())
+      .then(json => console.log(json))
+      .catch(err => console.log(err));
   }
 
   _renderMovies = () => {
